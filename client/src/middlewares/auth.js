268bcc7d0722f3c01a5564db4c98303baad32aee
Matheus-Mazepa <mfmazepa@hotmail.com>
2022-01-03 08:58:30 -0300

    bugfix name route login

--- client/src/middlewares/auth.js
+++ client/src/middlewares/auth.js
@@ -2,7 +2,7 @@ import Cookies from 'js-cookie';
 
 export default function auth({next, router}) {
   if (!Cookies.get('authorization')) {
-    return router.push({ name: 'Login' });
+    return router.push({ name: 'login' });
   }
 
   return next();
